c555e0f86c40f19c178882cac72a0509c9d310c9
Hardik Makwana <hardikmakwana1432@gmail.com>
2021-09-30 13:10:48 +0530

    displayed followers in DOM

--- assets/js/follower.js
+++ assets/js/follower.js
@@ -16,23 +16,54 @@ function getFollowers(username) {
 // fetch followers data
 getFollowers(username);
 
+// function to get follower's full information
+function getFollowerData(followerUsername) {
+    const url = `https://api.github.com/users/${followerUsername}`;
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log("Follower Api Data:", data);
+        let html = `
+        <div class="card bg-transparent border-secondary text-white mb-3">
+            <div class="row g-0">
+                <div class="col-md-2 d-flex justify-content-center align-items-center">
+                    <img src="${data.avatar_url}" class="img-fluid rounded-circle" alt="..." style="width: 100px;">
+                </div>
+                <div class="col-md-10">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.name}<i class="bi bi-patch-check text-warning ms-2"></i><small class="opacity-75 fw-light ms-3">${data.login}</small></h5>
+                        <p class="card-text text-truncate">${data.bio}</p>
+                        <div class="d-flex">
+                            <small class="opacity-75 me-4"><i class="bi bi-geo-alt-fill me-2"></i>${data.location}</small>
+                            <small class="opacity-75 me-3"><i class="bi bi-people-fill me-2"></i>${data.followers} Followers</small>
+                            <small class="opacity-75 me-3"><i class="bi bi-person-lines-fill me-2"></i>${data.following} Following</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+        return displayFollowers(html);
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
 // function to extrat followers data 
 function userFollowers(follower) {
     // console.log('Function called: userFollowers()');
+    // console.error(follower);
     console.log("\n");
     console.warn(`--- Followers fetched successfully!`);
-    
     // List of followers
+    let username, array = [];
     for(let i=0; i < follower.length; i++) {
-
         //* Follower object 
-        console.log(follower[i]);
-        
-        //* Extracted follower information from follower object
-        // console.log("-------------------------------");
-        // let followerEntries = Object.entries(follower[i]);
-        // for(let j=0; j < followerEntries.length; j++){
-        //     console.log(followerEntries[j][0], ":" , followerEntries[j][1]);
-        // }
+        username = follower[i].login;
+        getFollowerData(username);
     }
+}
+
+function displayFollowers(template) {
+    let followerContainer = document.getElementById('follower-container');
+    followerContainer.innerHTML += template;
 }
